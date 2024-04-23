238167b8765826e0563d8a7d1164c02bf497fd5a
Maryam <shabanzadeh1712@yahoo.com>
2024-04-23 14:54:33 +0200

    install fontawesome and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <Router>
+    <Routes>
+      <Route path="/"></Route>
+      <Route path="/wohmanDress"></Route>
+      <Route path="MenDress"></Route>
+      <Route path="ChildDress"></Route>
+      <Route path="PlasticItems"></Route>
+      <Route path ="Stantionery"></Route>
+    </Routes>
+   </Router>
       
     </>
   )
