76f8154e9182349b74f2f01ce2de1f3faa775c5f
Maryam <shabanzadeh1712@yahoo.com>
2024-05-12 23:39:20 +0200

    erzeugt different bild of components

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -1,26 +1,33 @@
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons/faShoppingCart";
+import menImage from "../../assets/men.jpeg"
+import kidImage from "../../assets/kid.jpeg"
+import womenImage from "../../assets/wohmen.jpeg"
+import stationeryImage from "../../assets/stationery.jpeg"
+import toysImage from "../../assets/toys.jpg"
 
 
-const Shop = ()=>{
-    return(
-        <div>
-            
-                <div className="m-4 rounded-full bg-gray-100 w-20 h-20 flex items-center justify-center">
-                <Link to="/cart">
-                    <FontAwesomeIcon icon={faShoppingCart} />
-                </Link >
-                </div >
-                <div className="m-4 rounded-full bg-gray-100 w-20 h-20 flex items-center justify-center">
-                    <Link to="/wohmenDress">
 
+const Shop = () => {
+    const categories = [
+        { name: "Men", image: menImage, link: "/menDress" },
+        { name: "Kids", image: kidImage, link: "/kidDress" },
+        { name: "Women", image: womenImage, link: "/womenDress" },
+        { name: "Stationery", image: stationeryImage, link: "/stationery" },
+        { name: "Toys", image: toysImage, link: "/toys" }
+    ];
+
+    return (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+            {categories.map((category, index) => (
+                <div key={index} className="m-4 rounded-full bg-gray-100 flex items-center justify-center">
+                    <Link to={category.link}>
+                        <img src={category.image} alt={category.name} className="w-40 h-40 sm:w-64 md:w-64 lg:w-40" />
+                        <div className="text-center mt-2">{category.name}</div>
                     </Link>
                 </div>
-                </div>
-            
-        
-    )
-}
+            ))}
+        </div>
+    );
+};
 
 export default Shop;
