bfb6062e0928e59b9e03c7090d9e948bc7e988cf
Maryam <shabanzadeh1712@yahoo.com>
2024-05-22 20:17:49 +0200

    create products of kitchen

---
+++ src/data/productkitchen.tsx
@@ -0,0 +1,36 @@
+import gonsonicMixer from "../assets/gosonci_mixer.jpg"
+import bushMixer from "../assets/bush_mixer.jpg"
+import electronicMasher from "../assets/electronic masher.jpg"
+import speaker from "../assets/speaker.jpg"
+import stealKettle from "../assets/steal_kettle.jpg"
+
+export const PORODUCTSKITCHEN = [{
+    id: 1,
+    productName: "Gonsonic Mixer",
+    price: 10000,
+    productImage: gonsonicMixer}
+  ,
+  {
+  id: 2,
+  productName: "Bush Mixer",
+  price: 10000,
+  productImage: bushMixer}
+,{
+id: 3,
+productName: "Electronic masher",
+price: 10000,
+productImage: electronicMasher},
+{
+id: 4,
+productName: "Speaker",
+price: 10000,
+productImage: speaker
+
+},
+{
+  id: 5,
+  productName: "Steel kettle",
+  price: 10000,
+  productImage: stealKettle
+  
+  }]
