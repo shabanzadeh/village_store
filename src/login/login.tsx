e2ef28e28b5af1886f3440a77bd871537c84aad7
Maryam <shabanzadeh1712@yahoo.com>
2024-05-26 19:56:37 +0200

    mangement error with usestate

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 const Login = () => {
+    const [error, setError] = useState(false);
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -14,11 +16,12 @@ const Login = () => {
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
                 </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <div className="flex items-center justify-between ">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
                         Sign In
                     </button>
                 </div>
+                {error &&<span className="text-red-500 mt-2 font-size-10"> Wrong email or password!</span>}
             </form>
         </div>
     )
